ed5d572e4e192ba12da5dfcd96a20078e7693926
poxity1012 <s11021108@gm.cyut.edu.tw>
2025-02-19 14:13:36 +0800

    Delete 自動答題練習/test directory

--- 自動答題練習/test/from watermarker.py
+++
@@ -1,9 +0,0 @@
-from watermarker.marker import add_mark
-import os
-
-files = os.listdir('image_2')
-for file in files:
-    file_path = os.path.join('image_2',file)
-    add_mark(file_path,mark='ddg4gg3g',color='#8B8B1B',size=21)
-
-
